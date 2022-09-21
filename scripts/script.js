0862415ac1475aa83c48b90a6c38179233593a1f
Sensinki <snnrkrk1@gmail.com>
2022-09-22 01:04:17 +0200

    :rocket: header en meer css toevoegen

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
 // JavaScript Document
 console.log("hi");
+
+var hamburgerMenuButton = document.querySelector("header button");
+
+
+hamburgerMenuButton.addEventListener("click", hamburgerMenuButtonKlik);
+
+
+function hamburgerMenuButtonKlik() {
+  
+    var hamburgerMenu = document.querySelector("header");
+    hamburgerMenu.classList.toggle("menuOpen");
+
+  
+}
