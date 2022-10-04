be8cde7afc7529ba33315e342394561361763665
Sensinki <107122047+Sensinki@users.noreply.github.com>
2022-10-04 08:00:37 +0200

    :poop: dark mode en andere dingen

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,20 @@
 console.log("hi");
 
 
-// Hamburger menu
+// HAMBURGER MENU
 // Navbar gaat naar beneden
+// https://codepen.io/shooft/pen/dymRMBZ?editors=0110
 
+// selectoren
 var hamburgerMenuButton = document.querySelector("header button");
 var hamburgerMenu = document.querySelector("header");
 
-
+// als het geklikt wordt, gebeurt function hamburgerMenuButtonKlik
 hamburgerMenuButton.addEventListener("click", hamburgerMenuButtonKlik);
 
 
 function hamburgerMenuButtonKlik() {
+    // class toevoegen aan elementen
     hamburgerMenu.classList.toggle("menuOpen");
     hamburgerMenuButton.classList.toggle("kruisje");
 }
